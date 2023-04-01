2cb5bdbd7b96d032e8d79d93a80d0f0d17dfe9aa
SkiMaskSKE <95718642+SkiMaskSKE@users.noreply.github.com>
2023-03-31 19:32:23 -0600

    4

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 function TopBanner() {
   return (
     <>
-      <div className="row">
+      <div className="row text-center">
         <div className="col-5">
-          <img src="./1.jpg" alt="Company Logo" />
+          <img
+            src="./1.jpg"
+            className="img-fluid rounded float-right"
+            alt="Joe Hilton"
+          />
         </div>
 
         <div className="col align-self-center text-start">
--- src/Pages/MyPodcast.tsx
+++ src/Pages/MyPodcast.tsx
@@ -1,5 +1,24 @@
 function MyPodcast() {
-  return <a href="https://baconsale.com/">Visit the Baconsale Website</a>;
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="card mx-auto" style={{ width: '18rem' }}>
+        <img src="./1.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Joe Hilton Podcast</h5>
+          <p className="card-text">
+            If you click the link bellow it will send you to Joe Hilton's
+            Podcast. Have Fun!!
+          </p>
+          <a
+            href="https://baconsale.com/"
+            className="btn btn-danger stretched-link"
+          >
+            Visit the Baconsale Website
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyPodcast;
